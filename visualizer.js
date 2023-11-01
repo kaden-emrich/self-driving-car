2b69a49c17898fdb6414e3c22b7aa87e7dbba7b5
Kaden Emrich <kadenemrich@gmail.com>
2023-11-01 13:46:45 -0400

    video 8

--- visualizer.js
+++ visualizer.js
@@ -6,10 +6,18 @@ class Visualizer {
         const width = ctx.canvas.width - margin * 2;
         const height = ctx.canvas.height - margin * 2;
 
-        Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height);
+        const levelHeight = height / network.levels.length;
+
+        for(let i = network.levels.length - 1; i >= 0; i--) {
+            const levelTop = top + lerp(height - levelHeight, 0, network.levels.length == 1 ? 0.5 : i / (network.levels.length - 1));
+
+            ctx.setLineDash([7, 3]);
+
+            Visualizer.drawLevel(ctx, network.levels[i], left, levelTop, width, levelHeight, i == network.levels.length - 1 ? ['🠉', '🠈', '🠊', '🠋'] : []);
+        }
     }
 
-    static drawLevel(ctx, level, left, top, width, height) {
+    static drawLevel(ctx, level, left, top, width, height, outputLabels) {
         const right = left + width;
         const bottom = top + height;
         
@@ -37,7 +45,7 @@ class Visualizer {
 
             ctx.beginPath();
             ctx.arc(x, bottom, nodeRadius * 0.6, 0, Math.PI * 2);
-            ctx.fillStyle = 'white';
+            ctx.fillStyle = getRGBA(inputs[i]);
             ctx.fill();
         }
 
@@ -47,17 +55,30 @@ class Visualizer {
             ctx.arc(x, top, nodeRadius, 0, Math.PI * 2);
             ctx.fillStyle = 'black';
             ctx.fill();
-
             ctx.beginPath();
             ctx.arc(x, top, nodeRadius * 0.6, 0, Math.PI * 2);
-            ctx.fillStyle = 'white';
+            ctx.fillStyle = getRGBA(outputs[i]);
             ctx.fill();
 
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.arc(x, top, nodeRadius * 0.8, 0, Math.PI * 2);
             ctx.strokeStyle = getRGBA(biases[i]);
+            ctx.setLineDash([3, 3]);
             ctx.stroke();
+            ctx.setLineDash([]);
+
+            if(outputLabels[i]) {
+                ctx.beginPath();
+                ctx.textAlign = 'center';
+                ctx.textBaseline = 'middle';
+                ctx.fillStyle = 'black';
+                ctx.strokeStyle = 'white';
+                ctx.font = (nodeRadius * 1) + "px Arial";
+                ctx.fillText(outputLabels[i], x, top + nodeRadius * 0.1);
+                ctx.lineWidth = 0.5;
+                ctx.strokeText(outputLabels[i], x, top + nodeRadius * 0.1);
+            }
         }
     }
 

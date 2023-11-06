624f28779dbbb2979ddfcfeab49ff261b17f07c7
Kaden Emrich <kadenemrich@gmail.com>
2023-11-06 14:00:25 -0500

    joe slalom

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const bigTimmy = '{"levels":[{"inputs":[0.5485376253504012,0.16580630472458646,0,0,0],"outputs":[1,1,1,0,1,1],"biases":[-0.04792117936298591,-0.30469651673313825,-0.12187147572138969,0.1003367973151069,-0.03722209112518983,-0.027760007473259823],"weights":[[0.37034996305512746,0.12084429132176626,-0.06768703556840994,-0.10790575721709925,0.11741356989267264,0.037618996257984656],[-0.082168359783305,-0.11235094182084406,-0.3793529848210658,-0.25054171793610663,-0.08993331450076637,0.33942126276016826],[-0.11997341343239179,0.09792670591337979,-0.08727921316345374,0.19072788257725198,-0.08414373063541988,-0.18257487626532123],[0.03444722052019307,0.026816385759853407,-0.000953538108490087,-0.06262594786583307,-0.30775006365102364,-0.20673047702126435],[0.13773589556432098,0.11962099896061976,0.13013590573111217,-0.02004678109335209,0.12926836352644333,-0.09660217143062876]]},{"inputs":[1,1,1,0,1,1],"outputs":[1,0,0,0],"biases":[0.04377722914311517,-0.07674371573443867,-0.012523057835949387,0.23141451221316656],"weights":[[0.06452813836439307,0.14726954609020712,-0.0328429194717225,0.053987170390813896],[0.2181155546498113,0.1780485423718522,0.13666714412421285,-0.03207456028819919],[-0.032687877304774696,0.01852861361445729,-0.22660422341009373,-0.18282535480655554],[0.05981189867107853,-0.31638946663893885,-0.07904451155741177,0.07787081019782337],[-0.06203183125927316,-0.30584912404920506,-0.11500231321446777,-0.2980000056270724],[-0.08508368924375838,-0.16525157806652546,0.014643060956927998,-0.08183107736037137]]}]}';
 
+const joeSlalom = '{"levels":[{"inputs":[0.18689150099780016,0,0,0,0.11307304403340146],"outputs":[0,1,0,0,0,1],"biases":[0.10569500056331288,-0.1355477599299185,0.01921313600193774,0.15793095796572584,-0.025973820845149445,-0.13551370980375244],"weights":[[-0.07415333981520986,-0.13960977987128262,-0.1209176989101017,-0.03147404581890412,-0.22801420102236644,0.03671332187397319],[-0.01275407682335853,0.24273261753530131,0.014996986242434375,0.0981024245511228,-0.15482870441566615,0.08164328622240355],[0.09077883921129644,-0.005431838078482095,-0.13236625442367914,0.22772890758879488,-0.007746078003659677,-0.05062309300606194],[-0.3293397691158102,0.2360156677109065,-0.11085855318320631,0.21023495446344284,0.1676495101850029,-0.05255889780686876],[0.0013446888709976838,0.030700712222484623,0.02255781353696074,-0.2029426981119538,0.07910793811388227,-0.2383734949457317]]},{"inputs":[0,1,0,0,0,1],"outputs":[1,0,1,0],"biases":[-0.22147063261931266,0.0006671960763221865,-0.21681592712799386,0.07455684195805219],"weights":[[-0.09263536509686798,0.158093808206833,-0.05486585460163273,-0.012832332354920036],[0.05341010838807256,-0.1466424182840866,-0.34649086082760383,0.0822099260464639],[0.0699993518864699,-0.078884431660912,-0.10083011191180097,-0.129509198688208],[0.0018044954023192956,-0.3074167268485193,0.0019758927388194014,0.06021144075866239],[0.14085447222340955,0.21707944949139846,-0.2633894759096886,0.11441443790145338],[-0.029790539517172684,0.08829406600078968,0.3140498659954903,-0.1844968491359054]]}]}';
+
 const carCanvas = document.getElementById('carCanvas');
 carCanvas.width = 200;
 const networkCanvas = document.getElementById('networkCanvas');
@@ -10,7 +12,8 @@ const networkCtx = networkCanvas.getContext('2d');
 const road = new Road(carCanvas.width / 2, carCanvas.width*0.9);
 
 var DEADLYLASERSPEED = 3.1;
-var deadlyLaserY = 300;
+var deadlyLaserStart = 300;
+var deadlyLaserY = deadlyLaserStart;
 
 var alwaysShowPrevBest = true;
 var prioritizePrevBest = true;
@@ -27,7 +30,7 @@ const TRAFFIC_SPEED = 0;
 
 var trafficAmount = 10;
 
-var useRandomTraffic = false;
+var trafficType = 'default';
 
 var trafficYStart = -100;
 var trafficYIncrement = -150;
@@ -47,6 +50,22 @@ var defaultTraffic = [
     new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
     new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
     new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor())
+];
+
+var sTraffic = [
+    new Car(road.getLaneCenter(0), -100, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -250, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(2), -250, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(0), -400, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -400, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -550, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(2), -550, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(0), -700, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(1), -850, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor()),
+    new Car(road.getLaneCenter(2), -850, 30, 50, "DUMMY", TRAFFIC_SPEED, getRandomColor())
+
 ];
 
 function generateRandomTraffic(amount) {
@@ -102,12 +121,17 @@ function generateRandomTraffic(amount) {
 }
 
 function getNewTraffic() {
-
-    if(useRandomTraffic) {
-        return generateRandomTraffic(trafficAmount);
-    }
-    else {
-        return defaultTraffic;
+    switch(trafficType) {
+        case 's':
+            return sTraffic;
+            break;
+        case 'random':
+            return generateRandomTraffic(trafficAmount);
+            break;
+        case 'default':
+        default:
+            return defaultTraffic;
+            break;
     }
 }
 
@@ -138,7 +162,7 @@ function generateCars(n) {
 }
 
 async function newGeneration() {
-    deadlyLaserY = 300;
+    deadlyLaserY = deadlyLaserStart;
     cars = generateCars(N);
     bestCar = cars[0];
 
@@ -179,7 +203,6 @@ async function quickTrain(gens) {
 
         let liveCars = 0;
         for(let i = 0; i < cars.length; i++) {
-            let deviation = Math.abs(cars[i].y - bestCar.y);
             cars[i].update(road.borders, traffic, [{ x: 0, y: deadlyLaserY }, { x: carCanvas.width, y: deadlyLaserY }]);
             if(!cars[i].damaged) {
                 liveCars++;
@@ -280,4 +303,9 @@ function animate(time) {
     if(!stopAnimaion) {
         requestAnimationFrame(animate);
     }
+}
+
+function slalomMode() {
+    trafficType = 's';
+    deadlyLaserStart = 600;
 }
